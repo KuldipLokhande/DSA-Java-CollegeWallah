74f01fd38da2210393a2b8a69b3ab9b454099c70
Kuldip Lokhande <kuldiplokhande2511@gmail.com>
2026-07-07 16:23:31 +0530

    Add recursion programs to print numbers from 1 to N and N to 1

---
+++ src/recursion_31/Print_nTo1.java
@@ -0,0 +1,26 @@
+package recursion_31;
+
+import java.util.Scanner;
+
+public class Print_nTo1 {
+
+	static void printDecreasing(int n) {
+		if (n == 1) {
+			System.out.println(n);
+			return;
+		}
+		System.out.println(n);
+		printDecreasing(n - 1);
+	}
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the value of n: ");
+		int n = sc.nextInt();
+
+		printDecreasing(n);
+	}
+
+}
