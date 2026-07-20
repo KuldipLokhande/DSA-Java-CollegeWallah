7804bc931a4f7bb263c8d0bad4b53df026090b69
Kuldip Lokhande <kuldiplokhande2511@gmail.com>
2026-07-20 18:30:53 +0530

    Add Bubble Sort algorithm implementation

---
+++ src/bubbleSort_41/BubbleSort.java
@@ -0,0 +1,29 @@
+package bubbleSort_41;
+
+public class BubbleSort {
+	public static void bubbleSort(int arr[]) {
+		
+
+		for (int i = 0; i < arr.length - 1; i++) {
+			for (int j = 0; j < arr.length - i - 1; j++) {
+
+				if (arr[j] > arr[j + 1]) {
+
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		int arr[] = { 5, 4, 1, 2, 3, 6, 0 };
+		bubbleSort(arr);
+
+		for (int num : arr) {
+			System.out.print(num + " ");
+		}
+	}
+
+}
